af886af291bf1045e631ed7f192839d09a0eb3e7
Ankit Tisawer <ankittisawer@Ankits-MacBook-Air.local>
2025-11-10 14:38:51 +0530

    update

--- project/database/factories/UserFactory.php
+++ project/database/factories/UserFactory.php
@@ -4,33 +4,20 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
     protected $model = User::class;
 
-    protected static ?string $password;
-
-    public function definition(): array
+    public function definition()
     {
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'phone' => $this->faker->phoneNumber(),
+            'title' => $this->faker->jobTitle(),
             'avatar_url' => 'https://i.pravatar.cc/150?u=' . uniqid(),
             'bio' => $this->faker->sentence(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
         ];
-    }
-
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
